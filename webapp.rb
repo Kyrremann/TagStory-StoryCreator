e01577f3b72ec8dafda9f6774dfdc7ceb1b5d65c
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2014-05-07 15:21:51 +0200

    Workin version of the story part, now working on the tag parts

--- webapp.rb
+++ webapp.rb
@@ -5,6 +5,8 @@ require 'json'
 
 require_relative 'lib/story.rb'
 
+$story = {}
+
 get '/' do
   haml :index
 end
@@ -14,6 +16,27 @@ get '/story/create' do
 end
 
 post '/story/create' do
-  p params
-  haml :createStory, :local => {:params => params}
+	# save params to session
+	if params[:addTag] then
+		$story = params
+		redirect '/story/create/tag'
+	else
+		haml :createStory, :local => {:params => params}
+	end
 end
+
+get '/story/create/tag' do
+	# load params from session
+	haml :createTag, :local => {:params => params}
+end
+
+post '/story/create/tag' do
+	# save params to session
+	if params[:story] then
+		# $story = params
+		redirect '/story/create'
+	else
+		haml :createStory, :local => {:params => params}
+	end
+end
+
