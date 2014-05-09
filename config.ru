eecb0c3cb0bf849cbc235268e414bd9b6bea57dd
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2014-05-09 16:11:28 +0200

    Making ready for testing on the Heroku server

--- config.ru
+++ config.ru
@@ -2,6 +2,6 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
  
-require File.expand_path(File.dirname(__FILE__) + '/app')
+require File.expand_path(File.dirname(__FILE__) + '/webapp')
  
 run Sinatra::Application
--- webapp.rb
+++ webapp.rb
@@ -6,11 +6,51 @@ require 'json'
 require_relative 'lib/Story.rb'
 require_relative 'lib/SessionHandler.rb'
 
+enable :sessions
+
+# Scopes are space separated strings
+SCOPES = [
+          'https://www.googleapis.com/auth/userinfo.email'
+         ].join(' ')
+
+unless G_API_CLIENT = ENV['G_API_CLIENT']
+  raise "You must specify the G_API_CLIENT env variable"
+end
+
+unless G_API_SECRET = ENV['G_API_SECRET']
+  raise "You must specify the G_API_SECRET env veriable"
+end
+
+def client
+  client ||= OAuth2::Client.new(G_API_CLIENT, G_API_SECRET, {
+                                  :site => 'https://accounts.google.com', 
+                                  :authorize_url => "/o/oauth2/auth", 
+                                  :token_url => "/o/oauth2/token"
+                                })
+end
+
+
 $story = {:parts => {}}
 $current_tag = 0
 
 get '/' do
   haml :index
+end
+
+get "/auth" do
+  redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri,:scope => SCOPES,:access_type => "offline")
+end
+
+get '/oauth2callback' do
+  access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+  session[:access_token] = access_token.token
+  @message = "Successfully authenticated with the server"
+  @access_token = session[:access_token]
+  
+  # parsed is a handy method on an OAuth2::Response object that will 
+  # intelligently try and parse the response.body
+  @email = access_token.get('https://www.googleapis.com/userinfo/email?alt=json').parsed
+  haml :success
 end
 
 get '/stories' do
@@ -57,4 +97,11 @@ post '/create/tag' do
   else
     haml :createTag, :locals => {:params => get_current_tag}
   end
+end
+
+def redirect_uri
+  uri = URI.parse(request.url)
+  uri.path = '/oauth2callback'
+  uri.query = nil
+  uri.to_s
 end
