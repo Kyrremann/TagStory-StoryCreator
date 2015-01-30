351ad2bc9d749888c8b672556df72a8d4c3d3960
Kyrre Havik Eriksen <kyrremann@gmail.com>
2015-01-31 00:38:36 +0100

    A new simplier version of Story Creator is on it's way

--- webapp.rb
+++ webapp.rb
@@ -5,83 +5,104 @@ require 'json'
 require 'uuid'
 require 'securerandom'
 
-require_relative 'lib/Story.rb'
-require_relative 'lib/SessionHandler.rb'
-require_relative 'lib/DatabaseHandler.rb'
-require_relative 'lib/JavascriptGenerator.rb'
-require_relative 'lib/Utils.rb'
-
-# enable :sessions
 use Rack::Session::Cookie, :secret => 'super_secret_key_that_should_be_an_env_variable'
-
-# Scopes are space separated strings
-SCOPES = [
-          'https://www.googleapis.com/auth/userinfo.email'
-         ].join(' https://www.googleapis.com/auth/userinfo.profile')
-
-unless G_API_CLIENT = ENV['G_API_CLIENT']
-  raise "You must specify the G_API_CLIENT env variable"
-end
-
-unless G_API_SECRET = ENV['G_API_SECRET']
-  raise "You must specify the G_API_SECRET env variable"
-end
 
 unless CLOUDANT_URL = ENV['CLOUDANT_URL']
   raise "You must specify the CLOUDANT_URL env variable"
-end
-
-def client
-  client ||= OAuth2::Client.new(G_API_CLIENT, G_API_SECRET, {
-                                  :site => 'https://accounts.google.com', 
-                                  :authorize_url => "/o/oauth2/auth", 
-                                  :token_url => "/o/oauth2/token"
-                                })
 end
 
 DB = "#{ENV['CLOUDANT_URL']}"
 
 before do
   content_type :html, 'charset' => 'utf-8'
-  # Add /story/<id> to list
-  unless allow_url()
-    unless has_access_token?
-      redirect "/auth"
-    end
-  end
 end
 
 get '/' do
   haml :index
 end
 
-get "/auth" do
-  redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri,:scope => SCOPES,:access_type => "offline")
+get '/login' do
+  "not implemented"
 end
 
-get '/oauth2callback' do
-  access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
-  session[:access_token] = access_token.token
-  p "Successfully authenticated with the server"
-  
-  # parsed is a handy method on an OAuth2::Response object that will 
-  # intelligently try and parse the response.body
-  session[:user_info] = access_token.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json').parsed
-  redirect '/mystories'
+# new story
+get '/mystories/wizard/story' do
+  haml :wizard_story
 end
 
-get '/help' do
-  haml :help
+post '/mystories/wizard/story' do
+  p params
+  if params["add_tag"] then
+    redirect '/mystories/wizard/story/tag'
+  else
+    redirect '/mystories/wizard/story'
+  end
 end
 
-get '/community' do
-  status 404
-  body "Not implemented yet"
+get '/mystories/wizard/story/tag' do
+  haml :wizard_tag
 end
+
+post '/mystories/wizard/story/tag' do
+  p params
+  if params["add_tag"] then
+    redirect '/mystories/wizard/story/tag'
+  elsif params["add_option"] then
+    redirect '/mystories/wizard/story/tag/option'
+  else
+    redirect '/mystories/wizard/story/tag'
+  end
+end
+
+get '/mystories/wizard/story/tag/option' do
+  haml :wizard_tag_option
+end
+
+post '/mystories/wizard/story/tag/option' do
+  p params
+  if params["add_tag"] then
+    redirect '/mystories/wizard/story/tag'
+  else
+    redirect '/mystories/wizard/story/tag/option'
+  end
+end
+
+# edit story
+get '/mystories/edit/story/:storyId/:tagId/:optionId' do | storyId, tagId, optionId |
+  @doc = RestClient.get("#{DB}/stories/#{storyId}")
+  @result = JSON.parse(@doc)
+  haml :wizard_tag_option, :locals => {
+    :params => @result["story"]["tags"][tagId]["options"][optionId],
+    :storyId => storyId,
+    :tagId => tagId,
+    :optionId => optionId,
+    :tags => @result["story"]["tags"]
+  }
+end
+
+get '/mystories/edit/story/:storyId/:tagId' do | storyId, tagId |
+  @doc = RestClient.get("#{DB}/stories/#{storyId}")
+  @result = JSON.parse(@doc)
+  haml :wizard_tag, :locals => {
+    :params => @result["story"]["tags"][tagId],
+    :storyId => storyId,
+    :tagId => tagId,
+    :tags => @result["story"]["tags"]}
+end
+
+get '/mystories/edit/story/:storyId' do | storyId |
+  @doc = RestClient.get("#{DB}/stories/#{storyId}")
+  @result = JSON.parse(@doc)
+  haml :wizard_story, :locals => {
+    :params => @result["story"],
+    :storyId => storyId
+  }
+end
+
 
 get '/stories' do
   @doc = RestClient.get("#{DB}/stories/_design/lists/_view/story_header")
-  @result = JSON.parse(@doc)
+  @result = JSON.parse(@doc)["rows"]
   haml :stories
 end
 
@@ -97,157 +118,4 @@ get '/story/:id/json' do | id |
   @result = JSON.parse(@doc)
   content_type :json, 'charset' => 'utf-8'
   @result.to_json
-end
-
-get '/story/:id' do | id |
-  @doc = RestClient.get("#{DB}/stories/#{id}")
-  @story = JSON.parse(@doc)["story"]
-  haml :story
-end
-
-get '/mystories' do
-  # @doc = RestClient.get("#{DB}/stories/_design/lists/_search/authors?q=" + get_name.replace_whitespace("%20"))
-  @doc = RestClient.get("#{DB}/stories/_design/lists/_search/authors?q=K*")
-  @result = JSON.parse(@doc)
-  haml :my_stories
-end
-
-get '/mystories/wizard/story' do
-  haml :wizard_story
-end
-
-get '/mystories/wizard/tag' do
-  haml :wizard_tag
-end
-
-get '/mystories/wizard/tag/camera' do
-  haml :wizard_tag_camera
-end
-
-get '/mystories/wizard/tag/camera/direction' do
-  haml :wizard_tag_camera_direction
-end
-
-get '/mystories/wizard/tag/quiz' do
-  haml :wizard_tag_quiz
-end
-
-get '/mystories/wizard/tag/direction' do
-  haml :wizard_tag_direction
-end
-
-get '/mystories/create/story' do
-  init_story
-  set_rev nil
-  redirect 'mystories/edit/story'
-end
-
-get '/mystories/create/tag' do
-  init_tag
-  redirect '/mystories/edit/tag'
-end
-
-get '/mystories/edit/tag/quiz' do
-  haml :quiz, :locals => {:params => get_quiz}
-end
-
-post '/mystories/edit/tag/quiz' do
-  save_quiz(params[:quiz])
-  if params[:add_quiz] then
-    add_question
-  end
-  redirect '/mystories/edit/tag/quiz'
-end
-
-get '/mystories/edit/tag/options' do
-  haml :options, :locals => {:params => get_options}
-end
-
-post '/mystories/edit/tag/options' do
-  # params.strip_empty! # TODO
-  save_options(params[:options])
-  if params[:add_option] then
-    add_option
-  end
-  
-  redirect '/mystories/edit/tag/options'
-end
-
-get '/mystories/edit/tag/options/delete/:id' do
-  delete_option(params[:id])
-  redirect '/mystories/edit/tag/options'
-end
-
-get '/mystories/edit/tag/:id' do | id |
-  unless is_a_tag_id(id.to_i) then
-    status 404
-    body "Can't find tag with id #{id}"
-  else
-    switch_to_tag!(id.to_s)
-    redirect '/mystories/edit/tag'
-  end
-end
-
-get '/mystories/edit/tag' do
-  haml :edit_tag, :locals => {:params => get_current_tag}
-end
-
-post '/mystories/edit/tag' do
-  merge_current_tag!(params)
-  if params[:story] then
-    redirect '/mystories/edit/story'
-  elsif params[:add_tag] then
-    redirect '/mystories/create/tag'
-  else
-    haml :edit_tag, :locals => {:params => get_current_tag}
-  end
-end
-
-get '/mystories/edit/story/:id' do | id |
-  if not is_a_story_id(id.to_i) then
-    status 404
-    body "Can't find story with id #{id}"
-  else
-    @doc = RestClient.get("#{DB}/stories/#{id}")
-    @jdoc = JSON.parse(@doc)
-    @story = @jdoc["story"]
-    load_story(id, @story)
-    set_rev(@jdoc["_rev"])
-    redirect '/mystories/edit/story'
-  end
-end
-
-get '/mystories/edit/story' do
-  haml :edit_story, :locals => {:params => get_story}
-end
-
-post '/mystories/edit/story' do
-  params.strip_empty!
-  params.delete("save")
-  merge_story!(params) # "author": "", "title": "",
-
-  if get_rev.nil? then
-    create_story_in_database!
-  else
-    save_story_to_database!
-  end
-
-  if params[:add_tag] then
-    params.delete("add_tag")
-    redirect '/mystories/create/tag'
-  else
-    haml :edit_story, :locals => {:params => get_story}
-  end
-end
-
-def redirect_uri
-  uri = URI.parse(request.url)
-  uri.path = '/oauth2callback'
-  uri.query = nil
-  uri.to_s
-end
-
-def is_active_url(url)
-  "active" if url.kind_of?(String) and request.path_info.eql? url\
-  or url.kind_of?(Array) and url.include? request.path_info
 end
