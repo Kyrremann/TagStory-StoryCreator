ec4c03a93927d4578acb28e9a6ecbfdaf86fcbfa
Kyrre Havik Eriksen <Kyrremann@gmail.com>
2015-09-21 22:40:15 +0200

    You can now save the story, and there are some simple validations, that can be esaily expanded on later

--- models/story.rb
+++ models/story.rb
@@ -5,4 +5,8 @@ class Story < ActiveRecord::Base
   has_many :images, foreign_key: 'belongs_to'
 
   # acts_as_taggable_on :genre, :keywords, :tag_types, :game_modes
+
+  validates_presence_of :title, :description, :author, :country, :city, :place
+  validates_inclusion_of :language, in: I18nData.languages, message: "%{value} is not a valid language"
+  validates_associated :tags
 end
--- routes/wizard_story.rb
+++ routes/wizard_story.rb
@@ -1,10 +1,15 @@
 class TagStoryApp < Sinatra::Application
   get '/wizard/story' do
     @story = Story.find(params[:sid])
-    unless @story
-      redirect '/'
-    end
-
     haml :'wizard/story'
+  end
+
+  post '/wizard/story' do
+    @story = Story.find(params[:sid])
+    if @story.update(params[:story])
+      redirect "wizard/story?sid=#{params[:sid]}"
+    else
+      haml :'wizard/story'
+    end
   end
 end
