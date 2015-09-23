2ed509addffa8a62b3a3cab1b4d9e571af736494
Kyrre Havik Eriksen <Kyrremann@gmail.com>
2015-09-23 20:33:00 +0200

    Fixed some validations problems with tag

--- models/tag.rb
+++ models/tag.rb
@@ -3,6 +3,7 @@ class Tag < ActiveRecord::Base
   has_many :travel_options
   has_many :images, foreign_key: 'belongs_to'
 
-  validates_presence_of :title, :description, :skippable, :endpoint
+  validates_presence_of :title, :description
+  validates :skippable, :endpoint, inclusion: { in: [true, false] }
   validates_inclusion_of :tag_type, in: %w{qr nfc gps}, message: "%{value} is not a valid tag type"
 end
--- routes/wizard_tag.rb
+++ routes/wizard_tag.rb
@@ -1,15 +1,16 @@
 class TagStoryApp < Sinatra::Application
   get '/wizard/tag' do
     @tag = Tag.find(params[:tid])
-    @story = Story.find(params[:sid])
+    @story = Story.find(@tag.story_id)
     haml :'wizard/tag'
   end
 
   post '/wizard/tag' do
     @tag = Tag.find(params[:tid])
     if @tag.update(params[:tag])
-      redirect "wizard/tag?sid=#{@tag.story_id}&tid#{@tag.id}"
+      redirect "wizard/tag?sid=#{@tag.story_id}&tid=#{@tag.id}"
     else
+      @story = Story.find(@tag.story_id)
       haml :'wizard/tag'
     end
   end
